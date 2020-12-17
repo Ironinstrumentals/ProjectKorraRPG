aaeff58003e0ed3bb090d1cb271138e2b0c6a8ad
David Schlenker <david.schlenker.webdev@gmail.com>
2020-12-17 12:43:32 -0700

    Working Levels
    Replaced with my modified version of the working levels fork

--- src/com/projectkorra/rpg/configuration/ConfigManager.java
+++ src/com/projectkorra/rpg/configuration/ConfigManager.java
@@ -1,10 +1,12 @@
 package com.projectkorra.rpg.configuration;
 
-import com.projectkorra.projectkorra.configuration.ConfigType;
+import java.io.File;
+import java.util.Arrays;
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
 
-import java.io.File;
+import com.projectkorra.projectkorra.configuration.ConfigType;
 
 public class ConfigManager {
 
@@ -19,32 +21,56 @@ public class ConfigManager {
 
 	public static void configCheck(ConfigType type) {
 		FileConfiguration config = rpgConfig.get();
+		
+		config.addDefault("ResetLevelOnElementChange", false);
 
-		config.addDefault("Avatar.AvatarStateOnFinalBlow", true);
+		config.addDefault("Avatar.AvatarStateOnFinalBlow", false);
 		config.addDefault("Avatar.CurrentAvatar", "");
 		config.addDefault("Avatar.AutoCycle.Enabled", true);
 		config.addDefault("Avatar.AutoCycle.Interval", 1800000L);
 
 		config.addDefault("ElementAssign.Enabled", true);
 		config.addDefault("ElementAssign.Default", "None");
-		config.addDefault("ElementAssign.Percentages.Earth", 0.205);
-		config.addDefault("ElementAssign.Percentages.Water", 0.205);
-		config.addDefault("ElementAssign.Percentages.Air", 0.205);
-		config.addDefault("ElementAssign.Percentages.Fire", 0.205);
-		config.addDefault("ElementAssign.Percentages.Chi", 0.18);
-
+		config.addDefault("ElementAssign.Percentages.Earth", 0.24);
+		config.addDefault("ElementAssign.Percentages.Water", 0.24);
+		config.addDefault("ElementAssign.Percentages.Air", 0.24);
+		config.addDefault("ElementAssign.Percentages.Fire", 0.24);
+		config.addDefault("ElementAssign.Percentages.Chi", 0.01);
+		//SubElement START
 		config.addDefault("SubElementAssign.Enabled", true);
+		//WATER
 		config.addDefault("SubElementAssign.Percentages.Blood", 0.01);
-		config.addDefault("SubElementAssign.Percentages.Combustion", 0.3);
-		config.addDefault("SubElementAssign.Percentages.Flight", 0.01);
 		config.addDefault("SubElementAssign.Percentages.Healing", 0.2);
-		config.addDefault("SubElementAssign.Percentages.Ice", 1);
-		config.addDefault("SubElementAssign.Percentages.Lava", 0.3);
-		config.addDefault("SubElementAssign.Percentages.Lightning", 0.5);
-		config.addDefault("SubElementAssign.Percentages.Metal", 0.7);
 		config.addDefault("SubElementAssign.Percentages.Plant", 0.7);
-		config.addDefault("SubElementAssign.Percentages.Sand", 1);
+		config.addDefault("SubElementAssign.Percentages.Ice", 1);
+		//EARTH
+		config.addDefault("SubElementAssign.Percentages.Metal", 0.3);
+		config.addDefault("SubElementAssign.Percentages.Lava", 0.2);
+		config.addDefault("SubElementAssign.Percentages.Sand", 0.5);
+		//FIRE
+		config.addDefault("SubElementAssign.Percentages.Combustion", 0.3);
+		config.addDefault("SubElementAssign.Percentages.Lightning", 0.5);
+		config.addDefault("SubElementAssign.Percentages.BlueFire", 0.01);
+		//AIR
+		config.addDefault("SubElementAssign.Percentages.Flight", 0.01);
 		config.addDefault("SubElementAssign.Percentages.Spiritual", 0.4);
+		//SubElement END
+		config.addDefault("LightChakraAttributes", Arrays.asList("Knockback::MULTIPLICATION", "Knockup::MULTIPLICATION", "Radius::ADDITION"));
+
+		config.addDefault("ChatColors.DEFAULT", "GRAY");
+		config.addDefault("ChatColors.NOVICE", "YELLOW");
+		config.addDefault("ChatColors.INTERMEDIATE", "AQUA");
+		config.addDefault("ChatColors.ADVANCED", "RED");
+		config.addDefault("ChatColors.MASTER", "GREEN");
+		
+		for (EntityType etype : EntityType.values()) {
+			if (etype.isAlive()) {
+				config.addDefault("MobDrops." + etype.toString() + ".XP", 25);
+				config.addDefault("MobDrops." + etype.toString() + ".DefiniteDrops", Arrays.asList("list abilities here"));
+				config.addDefault("MobDrops." + etype.toString() + ".RandomChance", 12);
+				config.addDefault("MobDrops." + etype.toString() + ".RandomDropTiers", Arrays.asList("NOVICE", "INTERMEDIATE", "ADVANCED", "MASTER"));
+			}
+		}
 
 		config.addDefault("Storage.engine", "sqlite");
 
